01c8faef1f317e34a99167e6a59e55baf3aea032
klynger <rafafhgkifh@gmail.com>
2018-09-10 21:38:22 -0300

    Adding styled-components and material-ui

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { injectGlobal } from 'styled-components';
 import Routes from './Routes';
+import theme from '../utils/theme';
+
+injectGlobal([`
+  html, body {
+    background-color: ${theme.palette.background.default};
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Robot', sans-serif;
+    min-height: 100%;
+  }
+
+  #root {
+    min-height: 100vh;
+  }
+`]);
 
 const App = () => {
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       <Routes />
-    </div>
+    </MuiThemeProvider>
   );
 };
-
 
 export default App;
