3c2ed0f9ecc6f977c69f97fb13c4ff6fb00585d5
klynger <rafael.dantas@ccc.ufcg.edu.br>
2018-09-18 14:48:33 -0300

    Basic layout of create institution

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ApplicationBar from './ApplicationBar';
 import MenuDrawer from './MenuDrawer';
-
+// import HomeRoutes from './HomeRoutes';
+import CreateInstitutionPage from '../CreateInstitutionPage';
+import { Button } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
 const Wrapper = styled.div`
   align-items: center;
@@ -16,34 +19,59 @@ const Wrapper = styled.div`
 
 
 const ContentContainer = styled.div`
+  bakcground-color: inherit;
+  display: flex;
+  margin: 7px;
+  width: 100%;
 `;
 
 const MainContainer = styled.div`
+  align-items: center;
+  background-color: inherit;
+  display: flex;
+  flex-direction: column;
+  margin-top: 70px;
+  width: 100%;
 `;
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      openDrawer: true,
+      openDrawer: false,
+      openCreateInstitution: false,
     };
 
-    // this.handleDrawerToggle.bind(this);
-    // this.handleActiveDrawerToggle.bind(this);
+    this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
+    this.handleActiveDrawerToggle = this.handleActiveDrawerToggle.bind(this);
+    this.handleCreateInstitutionToggle = this.handleCreateInstitutionToggle.bind(this);
   }
 
-  handleDrawerToggle = () => {
+  handleCreateInstitutionToggle() {
+    this.setState(
+      ({ openCreateInstitution }) => ({
+        openCreateInstitution: !openCreateInstitution,
+      }));
+  }
+
+  handleDrawerToggle() {
     this.setState(({ openDrawer }) => ({ openDrawer: !openDrawer }));
   }
 
-  handleActiveDrawerToggle = () => {
+  handleActiveDrawerToggle() {
     this.handleDrawerToggle();
   }
 
   render() {
-    // const { classes } = this.props;
-    const { openDrawer } = this.state;
+    const { classes } = this.props;
+    const { openDrawer, openCreateInstitution } = this.state;
 
     return (
       <Wrapper>
@@ -54,7 +82,18 @@ class Home extends Component {
             open={openDrawer}
           />
           <MainContainer>
-            Home works
+            <Button
+              className={classes.button}
+              aria-label="Create a new institution"
+              variant="extendedFab"
+              onClick={this.handleCreateInstitutionToggle}
+            >
+              Create a New Institution
+            </Button>
+            <CreateInstitutionPage
+              open={openCreateInstitution}
+              onClose={this.handleCreateInstitutionToggle}
+            />
           </MainContainer>
         </ContentContainer>
       </Wrapper>
@@ -62,8 +101,8 @@ class Home extends Component {
   }
 }
 
-// Home.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-export default Home;
+export default withStyles(styles)(Home);
