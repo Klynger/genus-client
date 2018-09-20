d2a18353ff065a8a62149050c93bfcaee85d07d0
klynger <rafafhgkifh@gmail.com>
2018-09-19 23:29:44 -0300

    Integrating listing page with redux

--- src/components/InstitutionPage/ListingPage/index.js
+++ src/components/InstitutionPage/ListingPage/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Fade } from '@material-ui/core';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
 import ListFormat from './ListFormat';
+import EmptyView from './EmptyView';
+import { ResponsiveTitle } from '../../utils/SharedComponents';
 
-const ListingPage = () => (
-  <Fade in>
-    <div>
-      Listing works
-      <ListFormat />
-    </div>
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+const ListingPage = ({ institutions }) => (
+  <Fade style={{ width: '100%' }} in>
+    <Container>
+      <ResponsiveTitle>Instituições</ResponsiveTitle>
+      {institutions.length > 0 ?
+        <ListFormat institutions={institutions} /> :
+        <EmptyView />
+      }
+    </Container>
   </Fade>
 );
 
-export default ListingPage;
+ListingPage.propTypes = {
+  institutions: PropTypes.array,
+};
+
+ListingPage.defaultProps = {
+  institutions: [],
+};
+
+function mapStateToProps({ institution }) {
+  return {
+    institutions: institution.allIds.map(id => institution.byId[id]),
+  };
+}
+
+export default connect(mapStateToProps)(ListingPage);
