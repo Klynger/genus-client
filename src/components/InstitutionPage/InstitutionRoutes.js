bb9ab5df8e980cdb1f3ce69a659925a14216d766
klynger <rafafhgkifh@gmail.com>
2018-10-04 20:20:58 -0300

    Styling LoadingPage

--- src/components/InstitutionPage/InstitutionRoutes.js
+++ src/components/InstitutionPage/InstitutionRoutes.js
@@ -2,26 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Loadable from 'react-loadable';
-import LoadingPage from '../LoadingPage';
 
 const AsyncCreatePage = Loadable({
   loader: () => import('./CreatePage'),
-  loading: LoadingPage,
+  loading: () => null,
 });
 
 const AsyncDetailsPage = Loadable({
   loader: () => import('./DetailsPage'),
-  loading: LoadingPage,
+  loading: () => null,
 });
-
-// const AsyncListingPage = Loadable({
-//   loader: () => import('./ListingPage'),
-//   loading: LoadingPage,
-// });
 
 const InstitutionRoute = ({ match }) => (
   <Switch>
-    {/* <Route path={match.path} exact component={AsyncListingPage} /> */}
     <Route path={`${match.path}/new`} component={AsyncCreatePage} />
     <Route path={`${match.path}/details`} component={AsyncDetailsPage} /> :
     <Redirect to={`${match.path}/details`}
