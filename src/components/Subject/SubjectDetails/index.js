1364e8868c05d1b33f16c4de1e4227e78a913c84
klynger <rafael.klynger@gmail.com>
2018-11-08 13:41:36 -0300

    Create DefaultContainerRoute

--- src/components/Subject/SubjectDetails/index.js
+++ src/components/Subject/SubjectDetails/index.js
@@ -1,43 +1,12 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
 import SubjectInfo from './SubjectInfo';
 import { Fade } from '@material-ui/core';
+import AddStudentDialog from './AddStudentDialog';
 import AddteacherDialog from './AddTeacherDialog';
 import React, { Component, Fragment } from 'react';
 import EditSubjectDialog from './EditSubjectDialog';
-import AddStudentDialog from './AddStudentDialog';
-
-const Container = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  padding: ${({ unit }) => unit || 8}px;
-
-  @media screen and (min-width: 1920px) {
-    width: calc(50% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-
-  @media screen and (min-width: 1280px) and (max-width: 1919px) {
-    width: calc(60% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-
-  @media screen and (min-width: 960px) and (max-width: 1279px) {
-    width: calc(70% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-
-  @media screen and (min-width: 700px) and (max-width: 959px) {
-    width: calc(80% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-
-  @media screen and (min-width: 600px) and (max-width: 699px) {
-    width: calc(85% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-
-  @media screen and (max-width: 599px) {
-    width: calc(95% - ${({ unit }) => (unit || 8) * 2}px);
-  }
-`;
+import DefaultContainerRoute from '../../utils/DefaultContainerRoute';
 
 /*
  * This component needs data that is fetched by other
@@ -114,7 +83,7 @@ class SubjectDetailsPage extends Component {
 
     return (
       <Fade in>
-        <Container>{toRender}</Container>
+        <DefaultContainerRoute>{toRender}</DefaultContainerRoute>
       </Fade>
     );
   }
