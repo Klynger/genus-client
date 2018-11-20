d952caacfc19d7ca0c70fae0bb9b585a7f6c8f6b
klynger <rafael.klynger@gmail.com>
2018-11-20 17:40:23 -0300

    Fix reply to reply error

--- src/actions/reply.js
+++ src/actions/reply.js
@@ -1,40 +1,29 @@
 import { replySchema } from '../models/schema';
 import { dispatchEntities } from '../utils/helpers';
 import { requestGraphql } from '../utils/HTTPClient';
+import { mutationCreateReply } from '../queryGenerators/replyMutations';
 import { ADD_REPLY_TO_DISCUSSION, ADD_REPLY_TO_REPLY } from './actionTypes';
-import { mutationCreateReply, mutationReplyToReply } from '../queryGenerators/replyMutations';
 
-export const replyToDiscussion = input => dispatch => {
+export const createReply = (input, isReplyToReply) => dispatch => {
   return requestGraphql(mutationCreateReply(input), localStorage.getItem('token')).then(res => {
     if (res.data.data && res.data.data.createReply) {
       dispatchEntities(res.data.data.createReply, dispatch, replySchema);
-      dispatch({
-        type: ADD_REPLY_TO_DISCUSSION,
-        payload: {
-          replyId: res.data.data.createReply.id,
-          discussionId: res.data.data.createReply.discussion.id,
-        },
-      });
-    }
-  });
-};
 
-export const replyToReply = input => dispatch => {
-  return requestGraphql(mutationReplyToReply(input), localStorage.getItem('token')).then(res => {
-    if (res.data.data && res.data.data.replyToReply) {
-      dispatchEntities(res.data.data.replyToReply, dispatch, replySchema);
+      if (isReplyToReply) {
+        dispatch({
+          type: ADD_REPLY_TO_REPLY,
+          payload: {
+            parentId: input.parentId,
+            childId: res.data.data.createReply.id,
+          },
+        });
+      }
+
       dispatch({
         type: ADD_REPLY_TO_DISCUSSION,
         payload: {
-          replyId: res.data.data.replyToReply.id,
-          discussionId: res.data.data.replyToReply.discussion.id,
-        },
-      });
-      dispatch({
-        type: ADD_REPLY_TO_REPLY,
-        payload: {
-          parentId: input.parentId,
-          childId: res.data.data.id,
+          discussionId: res.data.data.createReply.discussion.id,
+          replyId: res.data.data.createReply.id,
         },
       });
     }
