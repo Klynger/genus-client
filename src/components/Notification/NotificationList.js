84bea36c41c84c160bd5c897fa38a95bb10cbbbd
igorbrasileiro <brasileiro456@gmail.com>
2018-11-27 16:01:14 -0300

    fix component menu item behavior layout and change component construction

--- src/components/Notification/NotificationList.js
+++ src/components/Notification/NotificationList.js
@@ -1,43 +1,48 @@
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import React, { Fragment } from 'react';
 import classNames from 'classnames/bind';
 import { withRouter, Link } from 'react-router-dom';
 import { NO_USER_LOGGED } from '../../reducers/user';
-import React, { PureComponent, Fragment } from 'react';
 import { readNotification } from '../../actions/discussion';
 import { Divider, Menu, MenuItem, withStyles, withWidth } from '@material-ui/core';
 
-const NotificationActions = ({ classes, markAsRead }) => (
+const NotificationActions = ({ classes, markAsRead, readed }) => (
   <div className={classes.notificationActionsContainer}>
-    <span role="button" className={classes.markAsReadBtn} onMouseUp={markAsRead} tabIndex={0} />
+    <span
+      role="button"
+      className={readed ? classes.markAsunreadBtn : classes.markAsReadBtn}
+      onMouseUp={markAsRead}
+      tabIndex={0}
+    />
   </div>
 );
 
 NotificationActions.propTypes = {
   classes: PropTypes.object.isRequired,
   markAsRead: PropTypes.func.isRequired,
+  readed: PropTypes.bool.isRequired,
 };
 
 const styles = theme => ({
   menu: {
     maxHeight: '400px',
-    width: '100%',
-  },
-  menuItem: {
-    width: '100%',
-    padding: '10px 16px',
-    height: '45px',
     [theme.breakpoints.up('sm')]: {
       minWidth: '400px',
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down('xs')]: {
       minWidth: '250px',
     },
   },
   notificationWrapper: {
     display: 'flex',
     justifyContent: 'space-between',
-    width: 'calc(100% + 13px)',
+    width: 'calc(100% + 8px)',
+  },
+  menuItem: {
+    width: '100%',
+    padding: '10px 16px',
+    height: '45px',
   },
   notificationContent: {
     display: 'flex',
@@ -59,7 +64,16 @@ const styles = theme => ({
   },
   markAsReadBtn: {
     backgroundColor: 'rgba(0, 0, 0, 0.30)',
-    marginRight: '10px',
+    borderRadius: '50%',
+    height: '8px',
+    width: '8px',
+    position: 'relative',
+    '&:hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.54)',
+    },
+  },
+  markAsunreadBtn: {
+    backgroundColor: 'rgba(0, 0, 0, 0.30)',
     borderRadius: '50%',
     height: '8px',
     width: '8px',
@@ -82,93 +96,93 @@ const styles = theme => ({
   },
 });
 
-class NotificationList extends PureComponent {
-  getNotificationMessage = (message, width) => {
-    let len = message.length;
-    if (width === 'xs' && message.length >= 25) {
-      len = 25;
-    } else if (message.length >= 40) {
-      len = 40;
-    }
-    return message.length === len ? message : message.slice(0, len).concat('...');
-  };
-
-  render() {
-    const {
-      anchorEl,
-      classes,
-      menuId,
-      notifications,
-      onClose,
-      readNotificationById,
-      width,
-    } = this.props;
-    const open = Boolean(anchorEl);
-    const menuItemClasses = classNames.bind(classes);
-
-    return (
-      <Fragment>
-        <Menu
-          className={classes.menu}
-          disableAutoFocusItem
-          anchorEl={anchorEl}
-          id={menuId}
-          onClose={onClose}
-          open={open}
-        >
-          {notifications &&
-            notifications.map((notification, index) => (
-              <span>
-                {index === 0 && <Divider />}
-                <span className={classes.notificationWrapper} key={notification.id}>
-                  <MenuItem
-                    classes={{
-                      root: classes.menuItem,
-                    }}
-                    component={Link}
-                    to={`/institution/grade/${notification.gradeId}/subject/${
-                      notification.subjectId
-                    }/forum/discussion/${notification.discussionId}`}
-                    onClick={() => {
-                      onClose();
-                      if (!notification.read) {
-                        readNotificationById(notification.id);
-                      }
-                    }}
-                  >
-                    <div
-                      className={menuItemClasses('notificationContent', {
-                        readed: notification.read,
-                      })}
-                    >
-                      <span>
-                        {notification.notificationType === 'NEW_DISCUSSION'
-                          ? 'Nova Discussão'
-                          : 'Novo Comentário'}
-                      </span>
-                      <span className={menuItemClasses({ subHeader: !notification.read })}>
-                        {this.getNotificationMessage(notification.message, width)}
-                      </span>
-                    </div>
-                  </MenuItem>
-                  <NotificationActions
-                    classes={classes}
-                    markAsRead={() => {
-                      if (!notification.read) {
-                        readNotificationById(notification.id);
-                      }
-                    }}
-                  />
-                </span>
-                <Divider />
-              </span>
-            ))}
-          {notifications.length === 0 && <MenuItem disabled>Não Possui Notificação</MenuItem>}
-        </Menu>
-      </Fragment>
-    );
+const getNotificationMessage = (message, width) => {
+  let len = message.length;
+  if (width === 'xs' && message.length >= 17) {
+    len = 17;
+  } else if (message.length >= 35) {
+    len = 35;
   }
-}
+  return message.length === len ? message : message.slice(0, len).concat('...');
+};
+
+const NotificationList = ({
+  anchorEl,
+  classes,
+  menuId,
+  notifications,
+  onClose,
+  readNotificationById,
+  width,
+}) => {
+  const open = Boolean(anchorEl);
+  const menuItemClasses = classNames.bind(classes);
+
+  return (
+    <Fragment>
+      <Menu
+        classes={{
+          paper: classes.menu,
+        }}
+        disableAutoFocusItem
+        anchorEl={anchorEl}
+        id={menuId}
+        onClose={onClose}
+        open={open}
+      >
+        {notifications &&
+          notifications.map((notification, index) => (
+            <span key={notification.id}>
+              {index === 0 && <Divider />}
+              <span className={classes.notificationWrapper}>
+                <MenuItem
+                  classes={{
+                    root: classes.menuItem,
+                  }}
+                  component={Link}
+                  to={`/institution/grade/${notification.gradeId}/subject/${
+                    notification.subjectId
+                  }/forum/discussion/${notification.discussionId}`}
+                  onClick={() => {
+                    onClose();
+                    if (!notification.read) {
+                      readNotificationById(notification.id);
+                    }
+                  }}
+                >
+                  <div
+                    className={menuItemClasses('notificationContent', {
+                      readed: notification.read,
+                    })}
+                  >
+                    <span>
+                      {notification.notificationType === 'NEW_DISCUSSION'
+                        ? 'Nova Discussão'
+                        : 'Novo Comentário'}
+                    </span>
+                    <span className={menuItemClasses({ subHeader: !notification.read })}>
+                      {getNotificationMessage(notification.message, width)}
+                    </span>
+                  </div>
+                </MenuItem>
+                <NotificationActions
+                  classes={classes}
+                  markAsRead={() => {
+                    if (!notification.read) {
+                      readNotificationById(notification.id);
+                    }
+                  }}
+                  readed={notification.read}
+                />
+              </span>
+              <Divider />
+            </span>
+          ))}
+        {notifications.length === 0 && <MenuItem disabled>Não Possui Notificação</MenuItem>}
+      </Menu>
+    </Fragment>
+  );
+};
 
 NotificationList.propTypes = {
   anchorEl: PropTypes.object,
