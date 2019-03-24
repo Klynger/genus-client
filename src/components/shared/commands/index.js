51b0b21569bc712a1c205884ca30d616160e1d5e
igorbrasileiro <brasileiro456@gmail.com>
2019-03-24 16:38:43 -0300

    create Breakline Command and refactor icons

---
+++ src/components/shared/commands/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { commands } from 'react-mde';
+import breaklineCommand from './BreaklineCommand';
+import {
+  WrapText,
+  FormatBold,
+  FormatItalic,
+  StrikethroughS,
+  InsertLink,
+  FormatQuote,
+  InsertPhoto,
+  FormatListBulleted,
+  FormatListNumbered,
+  Title,
+} from '@material-ui/icons';
+
+const listCommands = [
+  {
+    commands: [
+      commands.headerCommand,
+      commands.boldCommand,
+      commands.italicCommand,
+      commands.strikeThroughCommand,
+    ],
+  },
+  {
+    commands: [commands.linkCommand, commands.quoteCommand, commands.imageCommand],
+  },
+  {
+    commands: [commands.orderedListCommand, commands.unorderedListCommand, breaklineCommand],
+  },
+];
+
+const getIcon = name => {
+  switch (name) {
+    case 'breakline':
+      return <WrapText />;
+    case 'bold':
+      return <FormatBold />;
+    case 'italic':
+      return <FormatItalic />;
+    case 'strikethrough':
+      return <StrikethroughS />;
+    case 'link':
+      return <InsertLink />;
+    case 'quote':
+      return <FormatQuote />;
+    case 'image':
+      return <InsertPhoto />;
+    case 'ordered-list':
+      return <FormatListNumbered />;
+    case 'unordered-list':
+      return <FormatListBulleted />;
+    default:
+      return <Title />;
+  }
+};
+
+export default {
+  getIcon,
+  listCommands,
+};
