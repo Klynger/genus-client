c540ce8806d7407d852015741fc5bd3b8c0d0546
klynger <rafael.klynger@gmail.com>
2019-04-12 15:46:30 -0300

    Create first version of ImageUplader component

--- src/components/Testing/index.js
+++ src/components/Testing/index.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import Image from './Image';
 import PropTypes from 'prop-types';
-import TestingRoutes from './TestingRoutes';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core';
 
-const Testing = ({ match }) => <TestingRoutes match={match} />;
+const styles = () => ({
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
+});
+
+class Testing extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      src: null,
+    };
+  }
+
+  handleImageChange = base64 => {
+    this.setState({ src: base64 });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { src } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Image src={src} alt="Test" onImageChange={this.handleImageChange} />
+      </div>
+    );
+  }
+}
 
 Testing.propTypes = {
-  match: PropTypes.shape({
-    path: PropTypes.string.isRequired,
-  }).isRequired,
+  classes: PropTypes.object.isRequired,
 };
 
-export default Testing;
+export default withStyles(styles)(Testing);
