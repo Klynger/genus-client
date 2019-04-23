77f0e8e9d2eefcabce76b0d112e692d6ef83ef1b
igorbrasileiro <brasileiro456@gmail.com>
2019-04-23 14:23:23 -0300

    creating SubjectInfoMenu

--- src/components/Subject/SubjectDetails/SubjectInfo.js
+++ src/components/Subject/SubjectDetails/SubjectInfo.js
@@ -1,11 +1,13 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Image from '../../shared/Image';
+import { MoreVert } from '@material-ui/icons';
+import SubjectMenuInfo from './SubjectInfoMenu';
+import React, { useState, Fragment } from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import { defaultImagesPaths } from '../../../utils/constants';
 import { ActionsContainer } from '../../shared/SharedComponents';
-import { Button, Paper, Typography, withStyles } from '@material-ui/core';
+import { Button, Paper, Typography, withStyles, IconButton } from '@material-ui/core';
 
 const PHOTO_DIMENSION = 150;
 
@@ -20,6 +22,7 @@ const styles = theme => ({
     marginTop: theme.spacing.unit * 3,
     padding: theme.spacing.unit,
     paddingTop: theme.spacing.unit * 3,
+    position: 'relative',
     width: `calc(100% - ${theme.spacing.unit * 2}px)`,
   },
   contentContainer: {
@@ -34,6 +37,14 @@ const styles = theme => ({
     flexDirection: 'column',
     flexWrap: 'wrap',
     padding: theme.spacing.unit,
+  },
+  menuIcon: {
+    margin: theme.spacing.unit,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    height: 48,
+    width: 48,
   },
 });
 
@@ -50,59 +61,71 @@ const SubjectInfo = ({
   canSeeForum,
   userRole,
 }) => {
+  const menuId = 'subject-info__menu';
+  const [menuState, setMenuState] = useState({ openMenu: false, menuAnchorEl: null });
   let image = null;
   if (subject.mimeType && subject.photo) {
     image = `${subject.mimeType},${subject.photo}`;
   }
 
+  const handleMenuClose = e => {
+    if (menuState.menuAnchorEl.contains(e.target)) {
+      return;
+    }
+    setMenuState({ openMenu: false, menuAnchorEl: null });
+  };
+
   return (
-    <Paper className={classes.root}>
-      <div className={classes.contentContainer}>
-        <div className={classes.imageContainer}>
-          <Image rounded={false} editable={false} src={image || defaultImagesPaths.SUBJECT} />
+    <Fragment>
+      <SubjectMenuInfo
+        onClose={handleMenuClose}
+        open={menuState.openMenu}
+        id={menuId}
+        anchorEl={menuState.menuAnchorEl}
+        isAdmin={isAdmin}
+        canSendEmail={userRole === 'TEACHER' || userRole === 'ADMIN'}
+        onEditSubjectOpen={onEditSubjectClick}
+        onAddTeacherOpen={onAddTeacherClick}
+        onAddStudentOpen={onAddStudentClick}
+        onSendEmailOpen={onSendEmailOpen}
+      />
+      <Paper className={classes.root}>
+        <IconButton
+          aria-haspopup="true"
+          className={classes.menuIcon}
+          onClick={evt => setMenuState({ menuAnchorEl: evt.currentTarget, openMenu: true })}
+        >
+          <MoreVert />
+        </IconButton>
+        <div className={classes.contentContainer}>
+          <div className={classes.imageContainer}>
+            <Image rounded={false} editable={false} src={image || defaultImagesPaths.SUBJECT} />
+          </div>
+          <div className={classes.infoContainer}>
+            <Typography component="h2" variant="h6" gutterBottom>
+              {subject.name}
+            </Typography>
+            <Typography component="span" variant="subtitle1" gutterBottom>
+              {subject.teachers.length > 0
+                ? `Professores: ${subject.teachers.map(({ username }) => username).join(', ')}`
+                : 'Nenhum professor vinculado a essa disciplina'}
+            </Typography>
+          </div>
         </div>
-        <div className={classes.infoContainer}>
-          <Typography component="h2" variant="h6" gutterBottom>
-            {subject.name}
-          </Typography>
-          <Typography component="span" variant="subtitle1" gutterBottom>
-            {subject.teachers.length > 0
-              ? `Professores: ${subject.teachers.map(({ username }) => username).join(', ')}`
-              : 'Nenhum professor vinculado a essa disciplina'}
-          </Typography>
-        </div>
-      </div>
-      <ActionsContainer>
-        {isAdmin && (
-          <span>
-            <Button color="primary" onClick={onEditSubjectClick}>
-              Atualizar Informações
+        <ActionsContainer>
+          {canSeeForum && (
+            <Button color="primary" component={Link} to={`${history.location.pathname}/forum`}>
+              Forum
             </Button>
-            <Button color="primary" onClick={onAddTeacherClick}>
-              Vincular professor
+          )}
+          {userRole === 'TEACHER' && (
+            <Button color="primary" onClick={onAddGradeClick}>
+              Adicionar nova nota
             </Button>
-            <Button color="primary" onClick={onAddStudentClick}>
-              Vincular aluno
-            </Button>
-          </span>
-        )}
-        {canSeeForum && (
-          <Button color="primary" component={Link} to={`${history.location.pathname}/forum`}>
-            Forum
-          </Button>
-        )}
-        {userRole === 'TEACHER' && (
-          <Button color="primary" onClick={onAddGradeClick}>
-            Adicionar nova nota
-          </Button>
-        )}
-        {(userRole === 'TEACHER' || userRole === 'ADMIN') && (
-          <Button color="primary" onClick={onSendEmailOpen}>
-            Enviar Email
-          </Button>
-        )}
-      </ActionsContainer>
-    </Paper>
+          )}
+        </ActionsContainer>
+      </Paper>
+    </Fragment>
   );
 };
 
