0ba1a14d28590c2543cf8bf3b15e279b9e949daf
Breno <jose.souza@splab.ufcg.edu.br>
2019-06-09 16:34:17 -0300

    Change https to http

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 const domain = {
   development: 'http://localhost:8080/graphql',
-  production: 'https://178.62.235.183:8080/graphql',
+  production: 'http://178.62.235.183:8080/graphql',
 };
 
 export default domain;
